1683d2dbb09e72b77af27ebb04829a2a155e9e95
Saravanakumar Damodharan <sdamodharan@sdl.com>
2019-02-26 13:13:41 +0530

    new code

---
+++ check_number.py
@@ -0,0 +1,12 @@
+x=int(input("enter the number"))
+
+if x==1:
+    print("number is one")
+elif x==2:
+    print("number is two")
+elif x==3:
+    print("number is three")
+elif x==0:
+    print("number is zero")
+else:
+    print("number is above three")
