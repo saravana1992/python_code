8d511ca8b5c756ec560841f123672ab5576567b7
Saravanakumar Damodharan <sdamodharan@sdl.com>
2019-02-27 11:06:23 +0530

    new code

---
+++ greatest_number.py
@@ -0,0 +1,18 @@
+x=int(input("enter the number of x:"))
+y=int(input("enter the number of y:"))
+z=int(input("enter the number of z1:"))
+if x > y:
+    if x > z:
+        print("number x is greatest:", x)
+    else:
+        print("number z is greatest:", z)
+elif y > z:
+    if y > x:
+        print("number y is greatest:", y)
+    else:
+        print("number z is greatest:", z)
+elif z > x:
+    if z > y:
+        print("number z is greatest:", z)
+    else:
+        print("number y is greatest:", y)
